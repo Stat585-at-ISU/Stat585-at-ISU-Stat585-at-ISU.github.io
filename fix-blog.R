374ede858ea282318246f505fedd414052e02275
srvanderplas <srvanderplas@gmail.com>
2023-01-30 12:43:38 -0600

    blog files

---
+++ fix-blog.R
@@ -0,0 +1,15 @@
+# This script is to clean up the blog files before posting
+
+# Remove all .git dirs in the blog
+dirs <- list.dirs("blog-2023/")
+gitdirs <- grep("\\.git(hub)?$", dirs)
+unlink(dirs[gitdirs], recursive = T, force = T)
+
+# Remove all license/README files
+extramd <- list.files("blog-2023", "(LICENSE|README).md", full.names = T, recursive = T)
+unlink(extramd)
+
+# Remove other unnecessary files
+otherfilepat <- "(create-file\\.png|\\.DS_Store|gitignore|Rproj)"
+otherfiles <- list.files("blog-2023", otherfilepat, full.names = T, recursive = T)
+unlink(otherfiles)
