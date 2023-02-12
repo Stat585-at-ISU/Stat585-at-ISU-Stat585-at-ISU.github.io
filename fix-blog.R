be49a197adb7d3edccfc0227fcc1a88a6b80618c
Heike Hofmann <hofmann@iastate.edu>
2023-02-12 02:18:38 -0600

    blog 3 major overhaul

--- fix-blog.R
+++ fix-blog.R
@@ -1,13 +1,40 @@
 # This script is to clean up the blog files before posting
+library(dplyr)
 
 # Remove all .git dirs in the blog
 dirs <- list.dirs("blog-2023/")
 gitdirs <- grep("\\.git(hub)?$", dirs)
 unlink(dirs[gitdirs], recursive = T, force = T)
 
+# remove all cache files
+cache <- grep("cache", dirs, value=TRUE)
+unlink(cache, recursive = T, force = T)
+
 # Remove all license/README files
-extramd <- list.files("blog-2023", "(LICENSE|README).md", full.names = T, recursive = T)
-unlink(extramd)
+licenses <- list.files("blog-2023", "LICENSE(|.md)", full.names = T, recursive = T)
+unlink(licenses)
+
+# any files called README.* are excluded from rendering in quarto automatically.
+# we actually want blog posts in README.Rmd files to show up, but only if there are no other
+# Rmd files in the folder:
+rmds <- list.files("blog-2023", pattern="Rmd", full.names = T, recursive = T)
+dframe <- data.frame(files=rmds)
+dframe <- dframe %>% mutate(
+  basename = basename(files),
+  dirname = dirname(files)
+)
+dframe <- dframe %>% group_by(dirname) %>% mutate(n = n())
+rename_readme <- dframe %>% ungroup() %>% filter(n == 1, basename=="README.Rmd") %>% select(files) %>% unlist()
+file.rename(rename_readme, gsub("README.Rmd", "index.Rmd", rename_readme))
+
+unlink_readme <- dframe %>% ungroup() %>% filter(n > 1, basename=="README.Rmd") %>% select(files) %>% unlist()
+unlink(unlink_readme)
+
+# fix naming issues
+rmds <- list.files("blog-2023", pattern="Rmd", full.names = T, recursive = T)
+base_safe <- make.names(basename(rmds))
+idx <- which(base_safe != basename(rmds))
+file.rename(rmds[idx], file.path(dirname(rmds), base_safe)[idx])
 
 # Remove other unnecessary files
 otherfilepat <- "(create-file\\.png|\\.DS_Store|gitignore|Rproj|\\.bak)"
